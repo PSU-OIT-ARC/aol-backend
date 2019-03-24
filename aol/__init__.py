4507f7d1ac3d690f7db03a5caf9900c7684a5f0b
Thom Linton <tlinton@pdx.edu>
2019-03-24 14:40:45 -0700

    Updates platform support.
    AOL officially supports Python >= 3.5.

---
+++ aol/__init__.py
@@ -0,0 +1,12 @@
+"""The Atlas of Oregon Lakes."""
+import sys
+
+if sys.version_info[0] == 3 and sys.version_info < (3, 5):
+    raise Exception('ORO requires Python version 3.5 or later.')
+
+
+__version__ = '1.4.0.dev0'
+__author__ = 'OIT Web & Mobile Team, Portland State University'
+__homepage__ = 'https://aol.research.pdx.edu'
+
+__all__ = ['__version__']
--- setup.py
+++ setup.py
@@ -1,19 +1,61 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+import sys
+import re
+import os
 
-VERSION = '1.4.0.dev0'
+if sys.version_info < (3, 5):
+    raise Exception('AOL requires Python versions 3.5 or later.')
 
+
+def get_requirements():
+    with open('requirements.txt', 'r') as requirements:
+        return [line.strip()
+                for line in requirements
+                if line and \
+                not line.startswith('#') and \
+                not line.startswith('--')]
+
+# Metadata extraction by parsing 'aol' module directly.
+#   https://github.com/celery/kombu/blob/master/setup.py
+re_meta = re.compile(r'__(\w+?)__\s*=\s*(.*)')
+re_doc = re.compile(r'^"""(.+?)"""')
+
+def add_default(m):
+    attr_name, attr_value = m.groups()
+    return ((attr_name, attr_value.strip("\"'")),)
+
+def add_doc(m):
+    return (('doc', m.groups()[0]),)
+
+pats = {re_meta: add_default, re_doc: add_doc}
+here = os.path.abspath(os.path.dirname(__file__))
+meta_fh = open(os.path.join(here, 'aol/__init__.py'))
+
+try:
+    meta = {}
+    for line in meta_fh:
+        if line.strip() == '# -eof meta-':
+            break
+        for pattern, handler in pats.items():
+            m = pattern.match(line.strip())
+            if m:
+                meta.update(handler(m))
+finally:
+    meta_fh.close()
 
 setup(
     name='psu.oit.arc.aol',
-    version=VERSION,
-    description='Atlas of Oregon Lakes',
-    author='PSU - OIT - ARC',
-    author_email='consultants@pdx.edu',
-    url='http://aol.research.pdx.edu/',
+    version=meta['version'],
+    description=meta['doc'],
+    author=meta['author'],
+    long_description=open('README.md').read(),
+    url=meta['homepage'],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+
     install_requires=[
         'psu.oit.wdt.emcee>=1.0.0.rc7,<1.1',
         'django~=1.11.14',
@@ -38,4 +80,11 @@ setup(
             'docker-compose'
         ]
     },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
