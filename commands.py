5c45f2379c230bed29835645d130d3c2455dff2f
Thom Linton <tlinton@pdx.edu>
2019-03-24 14:59:42 -0700

    Adds volume, media and database provisioning support.

--- commands.py
+++ commands.py
@@ -6,6 +6,7 @@ from emcee.app.config import LegacyAppConfiguration
 from emcee.app import app_configs
 from emcee import printer
 
+from emcee.commands.db import pg_dump, pg_restore
 from emcee.commands.deploy import deploy
 from emcee.commands.python import virtualenv, install
 from emcee.commands.django import manage, manage_remote
@@ -21,7 +22,9 @@ from emcee.deploy.django import Deployer as DjangoDeployer
 
 # from emcee.backends.dev.provision.db import provision_database as provision_database_local
 from emcee.backends.aws.infrastructure.commands import *
-from emcee.backends.aws.provision.db import provision_database
+from emcee.backends.aws.provision.db import provision_database, import_database
+from emcee.backends.aws.provision.volumes import provision_volume
+
 
 configs.load('default', 'commands.yml', YAMLCommandConfiguration)
 app_configs.load('default', LegacyAppConfiguration)
@@ -43,11 +46,46 @@ def provision_app(createdb=False):
     provision_python()
     provision_nginx()
 
+    # Initialize/prepare attached EBS volume
+    provision_volume(mount_point='/vol/store')
+
     if createdb:
         backend_options={'with_postgis': True,
                          'with_devel': True,
                          'extensions': ['hstore']}
         provision_database(backend_options=backend_options)
+
+
+# Loading data model will cause instantiation of 'ClearableImageInput' which
+# will require that the path '{remote.path.root}/media' exist and be readable
+# by {service.user} so it's execution must be delayed until media assets have
+# been imported.
+@command
+def provision_media_assets():
+    app_media_root = os.path.join(config.remote.path.root, 'media')
+    owner = '{}:{}'.format(config.iam.user, config.remote.nginx.group)
+
+    # Create media directory on EBS mount and link to app's media root
+    remote(('mkdir', '-p', '/vol/store/media'), sudo=True)
+    remote(('mkdir', '-p', app_media_root), sudo=True)
+    remote(('test', '-h', config.remote.path.media, '||',
+            'ln', '-sf', '/vol/store/media', config.remote.path.media), sudo=True)
+
+    # Set the correct permissions on generated assets
+    remote(('chown', '-R', owner, '/vol/store/media'), sudo=True)
+    remote(('chown', '-R', owner, app_media_root), sudo=True)
+
+    # Synchronize icons and assorted media assets:
+    archive_path = 'media.tar'
+    if os.path.exists(archive_path):
+        if not confirm("Synchronize media from '{}'?".format(archive_path)):
+            return
+
+        copy_file(archive_path, config.remote.path.media)
+        remote(('tar', 'xvf', archive_path, '&&',
+                'rm', archive_path),
+               cd=config.remote.path.media
+        )
 
 
 class AOLDeployer(DjangoDeployer):
