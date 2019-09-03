2c737042e759a4358f7792e6cfd4ff085d76afec
Thom Linton <tlinton@pdx.edu>
2019-09-03 16:14:50 -0700

    Revises construction of photo serializer.
    Targets schema supported by vue-gallery by using the 'caption'
    (possibly truncated) as the title and generating a description
    using the 'author' and 'taken_on' attributes.

--- aol/photos/serializers.py
+++ aol/photos/serializers.py
@@ -2,6 +2,8 @@ import logging
 
 from rest_framework import serializers
 
+from django.template.defaultfilters import truncatechars
+
 from . import models
 
 logger = logging.getLogger(__name__)
@@ -9,9 +11,19 @@ logger = logging.getLogger(__name__)
 
 class PhotoSerializer(serializers.ModelSerializer):
     href = serializers.FileField(source='file')
-    description = serializers.CharField(source='caption')
+    title = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Photo
         fields = ('pk', 'href',
-                  'author', 'description', 'taken_on')
+                  'title', 'description')
+
+    def get_title(self, obj):
+        return truncatechars(obj.caption, 30)
+
+    def get_description(self, obj):
+        elements = [obj.author or "Unknown"]
+        if obj.taken_on is not None:
+            elements.append(' - Taken on {}'.format(obj.taken_on))
+        return ''.join(elements)
