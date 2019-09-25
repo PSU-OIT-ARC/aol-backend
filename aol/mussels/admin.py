d033cd4a41a5b255b317ac6a6c2b2cb645686a59
Thom Linton <tlinton@pdx.edu>
2019-09-25 02:22:49 -0700

    Adds support for uploading observation datafiles.

--- aol/mussels/admin.py
+++ aol/mussels/admin.py
@@ -1,9 +1,39 @@
+import tempfile
+
+from django.views.generic.edit import FormView
+from django.core.management import call_command
+from django.contrib import messages
 from django.contrib import admin
+from django.conf.urls import url
+from django.urls import reverse
+from django import http, forms
 
 
 class MusselAdmin(admin.ModelAdmin):
     list_display = ('name', 'machine_name', 'is_scientific_name')
     list_filter = ('is_scientific_name',)
+
+
+class LoadMusselObservationForm(forms.Form):
+    datafile = forms.FileField()
+
+
+class LoadMusselObservationView(FormView):
+    template_name = 'admin/admin_action_intermediate_form.html'
+    form_class = LoadMusselObservationForm
+
+    def form_valid(self, form):
+        try:
+            datafile = tempfile.NamedTemporaryFile()
+            for chunk in form.cleaned_data['datafile'].chunks():
+                datafile.write(chunk)
+
+            call_command('load_psmfc_mussel_observations', datafile.name)
+            messages.success(self.request, "CSV has been loaded successfully")
+        except Exception as exc:
+            messages.error(self.request, "CSV was not loaded: {}".format(str(exc)))
+
+        return http.HttpResponseRedirect(reverse('admin:mussels_musselobservation_changelist'))
 
 
 class MusselObservationAdmin(admin.ModelAdmin):
@@ -14,3 +44,12 @@ class MusselObservationAdmin(admin.ModelAdmin):
 
     search_fields = ('lake__title', 'lake__reachcode')
     raw_id_fields = ('lake', 'mussel')
+
+    def get_urls(self):
+        return [
+            url(r'^load/$',
+                self.admin_site.admin_view(LoadMusselObservationView.as_view()),
+                {'current_app':self.admin_site.name},
+                name='load-mussel-csv'
+                )
+        ] + super().get_urls()
--- aol/plants/admin.py
+++ aol/plants/admin.py
@@ -1,10 +1,52 @@
+import tempfile
+
+from django.views.generic.edit import FormView
+from django.core.management import call_command
+from django.contrib import messages
 from django.contrib import admin
+from django.conf.urls import url
+from django.urls import reverse
+from django import http, forms
+
+from aol.plants import enums
 
 
 class PlantAdmin(admin.ModelAdmin):
     list_display = ('name', 'common_name', 'is_native', 'noxious_weed_designation')
     list_filter = ('is_native', 'noxious_weed_designation')
     search_fields = ('name', 'common_name')
+
+
+REPORTING_SOURCE_CHOICES = (
+    ('CLR', enums.REPORTING_SOURCE_CLR),
+    ('IMAP', enums.REPORTING_SOURCE_IMAP)
+)
+
+class LoadPlantObservationForm(forms.Form):
+    source = forms.ChoiceField(choices=REPORTING_SOURCE_CHOICES)
+    datafile = forms.FileField()
+
+
+class LoadPlantObservationView(FormView):
+    template_name = 'admin/admin_action_intermediate_form.html'
+    form_class = LoadPlantObservationForm
+
+    def form_valid(self, form):
+        try:
+            datafile = tempfile.NamedTemporaryFile()
+            for chunk in form.cleaned_data['datafile'].chunks():
+                datafile.write(chunk)
+
+            source = form.cleaned_data['source']
+            if source == enums.REPORTING_SOURCE_CLR:
+                call_command('load_clr_plant_observations', datafile.name)
+            elif source == enums.REPORTING_SOURCE_IMAP:
+                call_command('load_imap_plant_observations', datafile.name)
+            messages.success(self.request, "CSV has been loaded successfully")
+        except Exception as exc:
+            messages.error(self.request, "CSV was not loaded: {}".format(str(exc)))
+
+        return http.HttpResponseRedirect(reverse('admin:plants_plantobservation_changelist'))
 
 
 class PlantObservationAdmin(admin.ModelAdmin):
@@ -14,3 +56,12 @@ class PlantObservationAdmin(admin.ModelAdmin):
 
     search_fields = ('lake__title', 'lake__reachcode')
     raw_id_fields = ('lake', 'plant')
+
+    def get_urls(self):
+        return [
+            url(r'^load/$',
+                self.admin_site.admin_view(LoadPlantObservationView.as_view()),
+                {'current_app':self.admin_site.name},
+                name='load-plant-csv'
+                )
+        ] + super().get_urls()
