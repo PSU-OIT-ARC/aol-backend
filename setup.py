c05d9ca1c64d59dd0539760a9ab84efa974c794b
Thom Linton <tlinton@pdx.edu>
2020-11-20 14:49:51 -0800

    Updates package dependencies.

--- setup.py
+++ setup.py
@@ -58,10 +58,10 @@ setup(
 
     install_requires=[
         'pytz',
-        'dateparser~=0.7.6',
-        'beautifulsoup4~=4.8.1',
+        'dateparser~=1.0.0',
+        'beautifulsoup4~=4.9.3',
         'requests~=2.24.0',
-        'Pillow~=7.2.0',
+        'Pillow~=8.0.0',
         'psycopg2~=2.8.6',
         'pyshp~=2.1.0',
         'Shapely~=1.7.0',
@@ -70,11 +70,11 @@ setup(
         'django~=2.2.16',
         'djangorestframework~=3.11.1',
         'celery~=4.4.7',
-        'django-filter~=2.3.0',
+        'django-filter~=2.4.0',
         'social-auth-core~=3.3.3',
         'social-auth-app-django~=4.0.0',
         'django-ckeditor~=6.0.0',
-        'sentry-sdk~=0.17.4',
+        'sentry-sdk~=0.19.4',
     ],
     extras_require={
         'dev': [
