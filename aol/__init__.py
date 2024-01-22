b43eaff8e4b9eb6e0d62b9bc33073545f68b8d7f
Thom Linton <tlinton@pdx.edu>
2024-01-22 15:19:46 -0800

    Adds support for PEP 517, PEP 518, PEP 621, PEP 660.

--- aol/__init__.py
+++ aol/__init__.py
@@ -1,12 +1,6 @@
 """The Atlas of Oregon Lakes."""
-import sys
-
-if sys.version_info < (3, 7):
-    raise Exception('ORO requires Python version 3.7 or later.')
 
 
 __version__ = '1.6.10'
-__author__ = 'OIT Web & Mobile Team, Portland State University'
-__homepage__ = 'https://psu-arc-oit.github.io/aol-backend'
 
 __all__ = ['__version__']
